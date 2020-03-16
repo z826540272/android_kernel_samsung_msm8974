9c16e86690dab4b99f73e422a1317887ad6e80fd
Heiler Bemerguy <heiler.bemerguy@gmail.com>
2020-03-16 17:58:34 -0300

    is_vm_hugetlb_page is static but used in inline function vma_address which is not static

--- include/linux/hugetlb_inline.h
+++ include/linux/hugetlb_inline.h
@@ -12,7 +12,7 @@ static inline int is_vm_hugetlb_page(struct vm_area_struct *vma)
 
 #else
 
-static inline int is_vm_hugetlb_page(struct vm_area_struct *vma)
+inline int is_vm_hugetlb_page(struct vm_area_struct *vma)
 {
 	return 0;
 }
