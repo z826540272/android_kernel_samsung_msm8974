7d607bd14bae37027b769fe70378270f25ed2c5e
vilin <826540272@qq.com>
2020-03-27 00:41:12 +0800

    update build scripts

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 export ARCH=arm
-export CROSS_COMPILE=$(pwd)/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+#export CROSS_COMPILE=$(pwd)/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export CROSS_COMPILE=/home/vilin/UBERTC-arm-eabi-4.9-94cfd739eed6/bin/arm-eabi-
 
 mkdir output
 
-make -C $(pwd) O=output msm8974_sec_defconfig VARIANT_DEFCONFIG=msm8974pro_sec_klte_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-make -j64 -C $(pwd) O=output
+make -C $(pwd) O=output lineage_klte_pn547_defconfig
+make -C $(pwd) O=output menuconfig
+make -j5 -C $(pwd) O=output
+./mkdtb
+cp -f output/arch/arm/boot/dt.img .file_out/boot.emmc.win-dt
+cp -f output/arch/arm/boot/zImage .file_out/boot.emmc.win-zImage
 
-cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
